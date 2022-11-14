83720d6576dda5cfd81a277b76516145784a8368
DrWho23 <kaja.kalvas@gmail.com>
2022-11-14 23:53:48 +0100

    Přidání funkcí

--- sketch.js
+++ sketch.js
@@ -4,57 +4,79 @@ let print = document.getElementById("print")
 function main(){
     let dataArr = input.value.split(" ")
     
+    
     eval(dataArr)
 
     print.innerHTML = dataArr[0]
 }
 
 function eval(arr){
+    console.log("Postup řešení:")
+    display(arr)
+
     for (let i = 0; i < arr.length; i++){
-        
+        if(isFunction(arr[i])){
+            let ans = evalFunction(arr[i])
+            arr[i] = ""+ans
+
+            display(arr)
+        }
+    }
+
+    for (let i = 0; i < arr.length; i++){
+
         if(arr[i] == "*"){
-            console.log(arr)
             let ans = multiply(arr, i)
             arr.splice(i,i)
             arr.splice(i,i)
             arr[i-1] = ans
             i = 0
-            console.log(arr)
+
+            display(arr)
         }
 
         if(arr[i] == "/"){
-            console.log(arr)
             let ans = divide(arr, i)
             arr.splice(i,i)
             arr.splice(i,i)
             arr[i-1] = ans
             i = 0
-            console.log(arr)
+
+            display(arr)
         }
+
     }
 
     for (let i = 0; i < arr.length; i++){
-        
         if(arr[i] == "+"){
-            console.log(arr)
             let ans = add(arr, i)
             arr.splice(i,i)
             arr.splice(i,i)
             arr[i-1] = ans
             i = 0
-            console.log(arr)
+
+            display(arr)
         }
 
         if(arr[i] == "-"){
-            console.log(arr)
             let ans = subtract(arr, i)
             arr.splice(i,i)
             arr.splice(i,i)
             arr[i-1] = ans
             i = 0
-            console.log(arr)
+
+            display(arr)
         }
     }
+
+}
+
+function display(arr){
+    let result = ""
+    for(let i = 0; i < arr.length; i++){
+        result += " " + arr[i]
+    }
+    console.log(result)
 }
 
 function add(arr, i){
@@ -79,4 +101,49 @@ function divide(arr, i){
     let a = +arr[i-1]
     let b = +arr[i+1]
     return a / b
+}
+
+function isFunction(name){
+    let data = name.split(/[()]+/)
+
+    if(data[0] == "sqrt"){
+        return true
+    }
+    if(data[0] == "power"){
+        return true
+    }
+    if(data[0] == "sin"){
+        return true
+    }
+    if(data[0] == "cos"){
+        return true
+    }
+
+    return false
+}
+
+function evalFunction(name){
+    let data = name.split(/[()]+/)
+    let value = data[1]
+    let result
+
+    if(data[0] == "sqrt"){
+        result = Math.sqrt(value)
+    }
+    if(data[0] == "power"){
+        argument = data[1].split(",")
+        let x = +argument[0]
+        let exponent = +argument[1]
+
+        result = Math.pow(x, exponent)
+
+    }
+    if(data[0] == "sin"){
+        result = Math.sin(+data[1])
+    }
+    if(data[0] == "cos"){
+        result = Math.cos(+data[1])
+    }
+
+    return result
 }
