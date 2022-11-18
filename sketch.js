42faffcaa9d8340fe0457c4f70e8a0afc83662c7
DrWho23 <kaja.kalvas@gmail.com>
2022-11-18 01:38:27 +0100

    čištění

--- sketch.js
+++ sketch.js
@@ -4,12 +4,27 @@ let stepsLog = document.getElementById("steps")
 
 function main(){
     let dataString = input.value
-
-    let dataArr = new PrepareData(dataString).arr
-    let data = new EvalData(dataArr)
-
-    print.innerHTML = data.writeAnswer()
-    stepsLog.innerHTML = data.logSteps()
-
     
+    if(isEmpty(dataString)){
+
+        print.innerHTML = ""
+        stepsLog.innerHTML = ""
+
+    }else{
+
+        let dataArr = new PrepareData(dataString).arr
+        let data = new EvalData(dataArr)
+
+        print.innerHTML = data.writeAnswer()
+        stepsLog.innerHTML = data.logSteps()
+
+    }    
+}
+
+function isEmpty(string){
+    let data = string.replace(/\s/g, '')
+    if(data == ""){
+        return true
+    }
+    return false
 }
